cd7d5697476e87bbcf0eb39f28715e7bd6f0ecb4
Laura Luque <lauraluque1215@hotmail.com>
2024-08-03 21:13:28 -0500

    feat: Delete linkedin

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,22 +1,14 @@
 import styles from './styles.module.css'
 import React from 'react'
-import githubImg from '/icons/linkedin.svg'
-import linkedinImg from '/icons/github.svg'
+import githubImg from '/icons/github.svg'
 
 const Header: React.FC = () => {
   return (
     <header className={styles.header}>
       <p>Welcome!</p>
       <nav>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://www.linkedin.com/in/laura-luque1215/"
-        >
-          <img src={githubImg} alt="github" className={styles.social} />
-        </a>
         <a target="_blank" rel="noreferrer" href="https://github.com/MiauraDev">
-          <img src={linkedinImg} alt="linkedin" className={styles.social} />
+          <img src={githubImg} alt="linkedin" className={styles.social} />
         </a>
       </nav>
     </header>
